38db6f5042e782aa49f0b18549eda15fe6c5c49e
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2023-11-14 01:57:02 +0700

    making responsive dashboard

--- posyandu/src/app/dashboard/component/Header.js
+++ posyandu/src/app/dashboard/component/Header.js
@@ -5,8 +5,8 @@ import axios from "axios";
 
 const Header = () => {
   return (
-    <div className="flex flex-row bg-[#FF5757] h-12 justify-between items-center px-4 shadow-md shadow-neutral-400 relative left-auto right-auto">
-      <div className="flex items-center">
+    <div className="flex flex-row bg-[#FF5757] xl:bg-white h-12 xl:h-[80px] justify-between items-center px-4 shadow-md shadow-neutral-400 relative left-auto right-auto">
+      <div className="flex items-center ml-[20px]">
         <div className="drawer w-fit">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content">
@@ -14,7 +14,8 @@ const Header = () => {
             <label
               htmlFor="my-drawer"
             >
-              <img src="header/hamburger.svg"></img>
+              <img className="xl:hidden cursor-pointer" src="header/hamburger.svg"></img>
+              <img className="hidden xl:block cursor-pointer" src="header/hamburger-desktop.svg"></img>
             </label>
           </div>
           <div className="drawer-side">
@@ -23,7 +24,7 @@ const Header = () => {
               aria-label="close sidebar"
               className="drawer-overlay"
             ></label>
-            <ul className="menu p-4 w-[48vw] min-h-full bg-white text-base-content shadow shadow-neutral-400">
+            <ul className="menu p-4 w-[48vw] xl:w-[20vw] min-h-full bg-white text-base-content shadow shadow-neutral-400">
               {/* Sidebar content here */}
               <li className="mt-[40px] z-[-1]">
                 <a>Sidebar Item 1</a>
@@ -35,14 +36,21 @@ const Header = () => {
           </div>
         </div>
         {/* <img src="header/hamburger.svg"></img> */}
-        <img className="mr-1" src="header/logo.svg"></img>
-        <p className=" text-white text-[10px]">Posyandu Mawar I</p>
+        <img className="mr-1 xl:hidden" src="header/logo.svg"></img>
+        <img className="mr-1 hidden xl:block" src="header/logo-desktop.svg"></img>
+        <p className=" text-white font-semibold xl:text-neutral-700 text-[10px] xl:text-[16px]">Posyandu Mawar I</p>
       </div>
 
-      <div className="flex item-center">
-        <img src="header/search.svg"></img>
-        <img className="mx-2" src="header/filter.svg"></img>
-        <img src="header/profile.svg"></img>
+      <div className="flex item-center mr-[6px]">
+        <img className="xl:hidden" src="header/search.svg"></img>
+        <div className="hidden xl:flex items-center w-[260px] h-[35p] border-[2px] border-gray-400 rounded-[50px] shadow-md">
+        <img className="ml-[8px]" src="header/search.svg"></img> 
+        <input className="focus:outline-none placeholder:text-center placeholder:text-neutral-600 placeholder:text-sm ml-[5px] w-[200px]" placeholder="Pencarian" type="text"></input>
+        </div>
+        <img className="mx-2 xl:hidden cursor-pointer" src="header/filter.svg"></img>
+        <img className="mx-2 hidden xl:block cursor-pointer" src="header/filter-desktop.svg"></img>
+        <img className="xl:hidden cursor-pointer" src="header/profile.svg"></img>
+        <img className="hidden xl:block cursor-pointer" src="header/profile-desktop.svg"></img>
       </div>
     </div>
   );
--- posyandu/src/app/dashboard/page.js
+++ posyandu/src/app/dashboard/page.js
@@ -5,30 +5,35 @@ const DashboardScreen = () => {
   return (
     <div>
       <div className="flex flex-col items-center">
-        <div className="text-sm mt-10">
-          <div className="ml-2 w-[220px]">
-          <h1 className="text-[18px] font-bold">Dashboard</h1>
-          <p className="text-[10px] leading-[13px] font-semibold mt-2 mb-4">Berikut merupakan data statistik pasien di Posyandu Mawar I</p>
+        <div className="flex flex-col text-sm mt-10 xl:mt-[120px] justify-center">
+          <div className="ml-2 xl:w-[1400px]">
+          <h1 className="text-[18px] font-bold xl:text-[45px]">Dashboard</h1>
+          <p className="text-[10px] leading-[13px] font-semibold mt-2 mb-4 xl:text-[16px] xl:mt-[25px]">Berikut merupakan data statistik pasien di Posyandu Mawar I</p>
           </div>
 
-          <img className="w-50" src="dashboard/graph.svg"></img>
+          <img className="w-auto xl:hidden" src="dashboard/graph.svg"></img>
+          <img className="w-auto hidden xl:block" src="dashboard/graph-desktop.svg"></img>
         </div>
 
-        <div className="flex justify-between mt-9 w-[275px]">
+        <div className="flex justify-between mt-9 w-[275px] xl:w-[500px] mb-[100px]">
         <div className="dashboard-content">
-          <img className="w-[35px]" src="dashboard/bumil.svg"></img>
+          <img className="w-[35px] xl:w-[60px] xl:hidden" src="dashboard/bumil.svg"></img>
+          <img className="w-[35px] xl:w-[60px] hidden xl:block" src="dashboard/bumil-desktop.svg"></img>
           <p>Ibu Hamil</p>
         </div>
         <div className="dashboard-content">
-          <img className="w-[35px]" src="dashboard/baduta.svg"></img>
+          <img className="w-[35px] xl:w-[60px] xl:hidden" src="dashboard/baduta.svg"></img>
+          <img className="w-[35px] xl:w-[60px] hidden xl:block" src="dashboard/baduta-desktop.svg"></img>
           <p>Bayi</p>
         </div>
         <div className="dashboard-content">
-          <img className="w-[35px]" src="dashboard/balita.svg"></img>
+          <img className="w-[35px] xl:w-[60px] xl:hidden" src="dashboard/balita.svg"></img>
+          <img className="w-[35px] xl:w-[60px] hidden xl:block" src="dashboard/balita-desktop.svg"></img>
           <p>Balita</p>
         </div>
         <div className="dashboard-content">
-          <img className="w-[35px]" src="dashboard/lansia.svg"></img>
+          <img className="w-[35px] xl:w-[60px] xl:hidden" src="dashboard/lansia.svg"></img>
+          <img className="w-[35px] xl:w-[60px] hidden xl:block" src="dashboard/lansia-desktop.svg"></img>
           <p>Lansia</p>
         </div>
       </div>
