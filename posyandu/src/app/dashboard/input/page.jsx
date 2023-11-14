ca4c9bf8504cc44806df41886b29f8404a0255fb
Bendry <bendrylakburlawal01@gmail.com>
2023-11-15 01:13:33 +0700

    input 70%

--- posyandu/src/app/dashboard/input/page.jsx
+++ posyandu/src/app/dashboard/input/page.jsx
@@ -13,27 +13,143 @@ const InputScreen = () => {
       </div>
       <form action="" className="flex flex-col gap-[7px] text-[12px] my-2">
         <div className="flex gap-2 items-center">
-          <label className="w-[20%] text-end font-medium" htmlFor="">No. Urut <span className="text-red-500 absolute mt-[-6px]">*</span></label>
-          <input className="w-[80%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="urut" id="urut" />
+          <label className="w-[22%] text-end font-medium" htmlFor="">No. Urut <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="urut" id="urut" />
         </div>
         <div className="flex gap-2 items-center">
-          <label className="w-[20%] text-end font-medium" htmlFor="">NIK</label>
-          <input className="w-[80%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="urut" id="urut" />
+          <label className="w-[22%] text-end font-medium" htmlFor="">NIK</label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="nik" id="nik" />
         </div>
         <div className="flex gap-2 items-center">
-          <label className="w-[20%] text-end font-medium leading-[1.2]" htmlFor="">Nama Lengkap <span className="text-red-500 absolute mt-[-18px]">*</span></label>
-          <input className="w-[80%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="urut" id="urut" />
-        </div>
-        <div className="flex gap-2 items-center">
-          <label className="w-[20%] text-end font-medium leading-[1.2]" htmlFor="">Tanggal Lahir <span className="text-red-500 absolute mt-[-18px]">*</span></label>
-          <input className="w-[80%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="date" name="urut" id="urut" />
-        </div>
-        <div className="flex gap-2 items-center">
-          <label className="w-[20%] text-end font-medium leading-[1.2]" htmlFor="">Tanggal Lahir <span className="text-red-500 absolute mt-[-18px]">*</span></label>
-          <input className="w-[80%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="date" name="urut" id="urut" />
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Nama Lengkap <span className="text-red-500 absolute mt-[-18px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm" type="text" name="nama" id="nama" />
         </div>
 
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Tanggal Lahir <span className="text-red-500 absolute mt-[-18px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="date" name="ttl" id="ttl" />
+        </div>
+
+        <div className="flex gap-6 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Jenis Kelamin<span className="text-red-500 absolute mt-[-18px]">*</span></label>
+          <div className="flex flex-row gap-20">
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="jenkel" id="pria"/>
+              <label htmlFor="">L</label>
+            </div>
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="jenkel" id="wanita" />
+              <label htmlFor="">P</label>
+            </div>
+          </div>
+        </div>
         
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Nama Ibu <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="namaIbu" id="namaIbu" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">NIK Ibu <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="nikIbu" id="nikIbu" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Nama Ayah <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="namaAyah" id="namaAyah" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">NIK Ayah <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="namaAyah" id="namaAyah" />
+        </div>
+        
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Alamat <span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <textarea className="w-[78%] border-[1.5px] border-[#D5D8DE] rounded-sm pl-2 resize-none" name="alamat" id="alamat" rows="3"></textarea>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Alamat Domisili KK<span className="text-red-500 absolute mt-[-20px]">*</span></label>
+          <textarea className="w-[78%] border-[1.5px] border-[#D5D8DE] rounded-sm pl-2 resize-none" name="alamat" id="alamat" rows="3"></textarea>
+        </div>
+
+        <div className="flex gap-6 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Punya Buku KIA?<span className="text-red-500 absolute mt-[-18px]">*</span></label>
+          <div className="flex flex-row gap-20">
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="bukuKIA" id="iya"/>
+              <label htmlFor="">Iya</label>
+            </div>
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="bukuKIA" id="tidak" />
+              <label htmlFor="">Tidak</label>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">BB (kg)<span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="BB" id="BB" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">TB (kg)<span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="TB" id="TB" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Lingkar Kepala (cm)<span className="text-red-500 absolute mt-[-19px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="Lika" id="Lika" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Lingkar Lengan (cm)<span className="text-red-500 absolute mt-[-19px]">*</span></label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="Lila" id="Lila" />
+        </div>
+
+        <div className="flex gap-6 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Vitamin<span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <div className="flex flex-row gap-20">
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="vitamin" id="iya1"/>
+              <label htmlFor="">Iya</label>
+            </div>
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="vitamin" id="tidak1" />
+              <label htmlFor="">Tidak</label>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-6 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Imunisasi<span className="text-red-500 absolute mt-[-6px]">*</span></label>
+          <div className="flex flex-row gap-12">
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="imunisasi" id="DPT"/>
+              <label htmlFor="">DPT-HB-Hib</label>
+            </div>
+            <div className="flex gap-2">
+              <input className="accent-pink-500" type="radio" name="imunisasi" id="campak" />
+              <label htmlFor="">Campak</label>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Pelayanan Anak Pra Sekolah</label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="praSek" id="praSek" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Meninggal</label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="meninggal" id="meninggal" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <label className="w-[22%] text-end font-medium leading-[1.2]" htmlFor="">Keterangan</label>
+          <input className="w-[78%] h-9 border-[1.5px] border-[#D5D8DE] rounded-sm pl-2" type="text" name="keterangan" id="keterangan" />
+        </div>
       </form>
     </div>
   );
