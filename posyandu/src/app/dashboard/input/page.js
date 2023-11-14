3e63f40c8e8c14e974ebc900807f2d1f8193f246
Bendry <bendrylakburlawal01@gmail.com>
2023-11-14 11:22:05 +0700

    input

---
+++ posyandu/src/app/dashboard/input/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import Header from "./component/Header";
+
+const InputScreen = () => {
+  return (
+    <div className="bg-red-600">JUAN CINA</div>
+  );
+};
+
+export default InputScreen;
