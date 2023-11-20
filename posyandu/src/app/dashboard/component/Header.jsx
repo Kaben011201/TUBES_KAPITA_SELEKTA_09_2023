3bbc2b08d490aee847309721b52575f318a6f5d9
Bendry <bendrylakburlawal01@gmail.com>
2023-11-21 00:43:16 +0700

    onlclik setting

--- posyandu/src/app/dashboard/component/Header.jsx
+++ posyandu/src/app/dashboard/component/Header.jsx
@@ -269,7 +269,7 @@ const Header = () => {
             tabIndex={0}
             className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
           > 
-            <li>
+            <li onClick={()=>{router.push("/dashboard/setting")}}>
               <a>Settings</a>
             </li>
             <li onClick={()=>{router.push("/login")}}>
