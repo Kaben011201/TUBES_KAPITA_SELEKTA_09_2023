cdb1d6cad62926e182433bb62e9ce58bb2b04d32
Juan Verrel Tanuwijaya <126883141+Juan-121140072@users.noreply.github.com>
2023-11-27 18:32:37 +0700

    fix bb5

--- posyandu/src/app/dashboard/component/Bulan.jsx
+++ posyandu/src/app/dashboard/component/Bulan.jsx
@@ -1,18 +1,19 @@
 const Bulan = () => {
   return (
     <select className="select select-bordered select-xs xl:select-sm mb-4 max-w-xs mt-[15px] mr-[10px]">
-      <option selected>Januari</option>
-      <option>Febuari</option>
-      <option>Maret</option>
-      <option>April</option>
-      <option>Mei</option>
-      <option>Juni</option>
-      <option>Juli</option>
-      <option>Agustus</option>
-      <option>September</option>
-      <option>Oktober</option>
-      <option>November</option>
-      <option>Desember</option>
+      <option selected value={0}>-Pilih Bulan-</option>
+      <option value={1} >Januari</option>
+      <option value={2}>Febuari</option>
+      <option value={3}>Maret</option>
+      <option value={4}>April</option>
+      <option value={5}>Mei</option>
+      <option value={6}>Juni</option>
+      <option value={7}>Juli</option>
+      <option value={8}>Agustus</option>
+      <option value={9}>September</option>
+      <option value={10}>Oktober</option>
+      <option value={11}>November</option>
+      <option value={12}>Desember</option>
     </select>
   );
 };
