d62025a1cc49c1e3ec59cff8c47ee623f8c72bc1
Bendry Lakburlawal <bendrylakburlawal01@gmail.com>
2023-12-13 19:55:36 +0700

    new

--- posyandu/src/middleware.js
+++ posyandu/src/middleware.js
@@ -18,11 +18,11 @@ export default async function middleware(req) {
       // console.log("Verified Token:", verifiedToken);
     } catch (error) {
       console.error("Verification failed:", error);
-      return NextResponse.redirect(`${deployUrl}/login`);
+      return NextResponse.redirect(`${url}/login`);
     }
   } else {
     console.log("No Token Found");
-    return NextResponse.redirect(`${deployUrl}/login`);
+    return NextResponse.redirect(`${url}/login`);
   }
 }
 
