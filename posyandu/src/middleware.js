c31467190b91407f879b4b2cb36026b15d36ef20
Arya Yudhistira <145851395+ryudhis@users.noreply.github.com>
2023-12-13 20:10:17 +0700

    Switch URL

--- posyandu/src/middleware.js
+++ posyandu/src/middleware.js
@@ -6,8 +6,8 @@ export default async function middleware(req) {
   // GET TOKEN
   const token = req.cookies.get("token")?.value;
   const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  const url = "http://localhost:3000";
-  //const url="https://posyandu-mawar-1.vercel.app";
+  //const url = "http://localhost:3000";
+  const url = "https://posyandu-mawar-1.vercel.app";
 
   // VERIFY TOKEN
   if (token) {
